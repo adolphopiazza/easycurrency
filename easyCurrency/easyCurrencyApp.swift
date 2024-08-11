4e18b471135907e58b75c706e93d74abfbb45922
Adolpho Piazza <adolphopiazza@gmail.com>
2024-08-11 20:36:47 -0300

    Initial commit

---
+++ easyCurrency/easyCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyCurrencyApp.swift
+//  easyCurrency
+//
+//  Created by Adolpho Francisco Zimmermann Piazza on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct easyCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
